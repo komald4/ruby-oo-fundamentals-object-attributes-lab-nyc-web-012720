a0e85df6e17fc4616e885a39fddae2e4c30d11fb
calm-fork-7535 <komald.4@gmail.com>
2020-01-29 18:44:14 +0000

    Automatically backed up by Learn

--- lib/person.rb
+++ lib/person.rb
@@ -1,41 +1,18 @@
 
-class Person
 
+
+
+class Person
+  def name=(name)
+    @name=name
+  end
   def name
     @name
   end
-
-  def name=(name)
-    @name = name
+  def jobs=(jobs)
+    @jobs=jobs
   end
-
   def jobs
     @jobs
-  end
-
-  def jobs
-    =(jobs
-    )
-    @jobs
-     = jobs
-     
   end
 end
-
-
-
-# class Person
-#   def name=(name)
-#     @name=name
-#   end
-#   def name
-#     @name
-#   end
-#   def jobs
-=(job)
-#     @job=job
-#   end
-#   def job
-#     @job
-#   end
-# end
